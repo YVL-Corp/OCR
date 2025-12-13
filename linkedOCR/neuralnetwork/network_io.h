b9dfbd5aec9e57827e41b3a7bce916bb2a18628c
tristaing <118687016+poulpipoz@users.noreply.github.com>
2025-12-13 22:00:42 +0100

    fixed comments blabla

--- linkedOCR/neuralnetwork/network_io.h
+++ linkedOCR/neuralnetwork/network_io.h
@@ -3,10 +3,8 @@
 
 #include "neural_network.h"
 
-// Sauvegarde le réseau de neurones dans un fichier binaire
 int save_network(Network *net, const char *filename);
 
-// Charge un réseau de neurones depuis un fichier binaire
 Network* load_network(const char *filename);
 
-#endif // NETWORK_IO_H
+#endif
