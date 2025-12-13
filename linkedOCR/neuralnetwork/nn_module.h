0cb7ab00218eb772ba8f0aff0a405ae33f12a5ed
tristaing <118687016+poulpipoz@users.noreply.github.com>
2025-12-13 21:58:47 +0100

    removed comments (unnecessary the function explains itself well)

--- linkedOCR/neuralnetwork/nn_module.h
+++ linkedOCR/neuralnetwork/nn_module.h
@@ -1,9 +1,4 @@
 #ifndef NN_MODULE_H
 #define NN_MODULE_H
 
-// Fonction principale pour lancer la reconnaissance optique de caractères (OCR)
-// Lit les dossiers d'images et génère grid.txt et words.txt
-// Retourne 0 si succès, 1 si erreur
 int nn_run_recognition(const char *root_folder, const char *model_file);
-
-#endif // NN_MODULE_H
